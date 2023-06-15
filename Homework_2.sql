e26864e88747d79c9b72f885e71de75c4d7e9957
BackDad <manuk_1122@mail.ru>
2023-06-15 21:41:32 +0300

    Homework2 done

---
+++ Homework_2.sql
@@ -0,0 +1,49 @@
+USE shop_base;
+DROP TABLE IF EXISTS sales;
+CREATE TABLE sales
+(
+id INT PRIMARY KEY AUTO_INCREMENT,
+order_date DATE,
+count_product INT);
+SELECT * from sales;
+INSERT sales (order_date,count_product)
+VALUES
+(
+"2022-01-01",156),
+("2022-01-02",180),
+("2022-01-03",21),
+("2022-01-04",124),
+("2022-01-05",341);
+SELECT * from sales;
+SELECT 
+id as 'ID Заказа',
+IF (count_product <100, 'Малый заказ',
+IF(count_product BETWEEN 100 AND 300, 'Средний заказ',
+IF (count_product > 300, 'Крупный заказ','Не определено')
+)
+)as 'Тип заказа'
+FROM sales;
+DROP TABLE IF EXISTS orders;
+CREATE TABLE orders
+(
+id INT PRIMARY KEY AUTO_INCREMENT,
+employee_id VARCHAR(3),
+amount FLOAT,
+order_status VARCHAR(10)
+);
+INSERT orders(employee_id,amount,order_status)
+VALUES
+("e03",15.00,"OPEN"),
+("e01",25.50,"OPEN"),
+("e05",100.70,"CLOSED"),
+("e02",22.18,"OPEN"),
+("e04",9.50,"CANCELLED");
+SELECT * FROM orders;
+SELECT id,
+IF (order_status = 'OPEN', 'Order is in open state',
+IF (order_status = 'CLOSED','Order is closed',
+IF (order_status = 'CANCELLED','Order is cancelled', 'Error'
+))) 
+as full_order_status
+FROM orders;
+
